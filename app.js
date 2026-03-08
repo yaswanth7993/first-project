3d82695206185ac62a567a0e7ef0e28b9effe614
yaswanth7993 <chinthayaswanth01@gmail.com>
2026-03-08 10:58:40 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,4 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => res.send('<h1>DevOps Project Success!</h1>'));
+app.listen(3000, () => console.log('Server Ready'));
